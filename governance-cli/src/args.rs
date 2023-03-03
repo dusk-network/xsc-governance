2bb115ed0f56a5981cd4b00ae5c672f21db9eca3
Herr Seppia <seppia@dusk.network>
2023-03-03 11:52:32 +0100

    governance-cli: use `--profile` to get wallet and config
    Resolves #7

--- governance-cli/src/args.rs
+++ governance-cli/src/args.rs
@@ -8,7 +8,7 @@ use clap::Parser;
 pub struct Args {
     /// Directory to store user data [default: `$HOME/.dusk/rusk-wallet`]
     #[clap(short, long)]
-    profile: PathBuf,
+    pub profile: PathBuf,
     /// Set the password for wallet's creation
     #[clap(long, env = "RUSK_WALLET_PWD")]
     pub password: String,
